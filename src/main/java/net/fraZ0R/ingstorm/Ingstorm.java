0aba88a4963b56fc3ab431f77260dd443bde35dd
oliver <kookooloopdaking@gmail.com>
2022-08-17 18:26:35 -0500

    Datapack-ify

--- src/main/java/net/fraZ0R/ingstorm/Ingstorm.java
+++ src/main/java/net/fraZ0R/ingstorm/Ingstorm.java
@@ -1,15 +1,15 @@
 package net.fraZ0R.ingstorm;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Blocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Ingstorm implements ModInitializer {
-
-	public static final Logger LOGGER = LoggerFactory.getLogger("ingstorm");
+	public static final String modid = "ingstorm";
+	public static final Logger LOGGER = LoggerFactory.getLogger(modid);
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Metroid Prime 2 :)");
+		LOGGER.info("Initializing "+modid);
 	}
 }
--- src/main/java/net/fraZ0R/ingstorm/mixin/MixinPlayerEntity.java
+++ src/main/java/net/fraZ0R/ingstorm/mixin/MixinPlayerEntity.java
@@ -1,12 +1,18 @@
 package net.fraZ0R.ingstorm.mixin;
 
+import net.fraZ0R.ingstorm.Ingstorm;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,19 +24,22 @@ import net.fraZ0R.ingstorm.common.BlockProximity;
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity extends LivingEntity {
 	@Shadow public abstract void playSound(SoundEvent sound, float volume, float pitch);
+	@Unique int counter = 0;
+	@Unique TagKey<Biome> HOT = TagKey.of(Registry.BIOME_KEY, new Identifier(Ingstorm.modid, "hot"));
 
 	protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
 	}
-	public int counter = 0;
+
+
 	@Inject(method = "tick", at = @At("HEAD"))
 	private void doNetherDamage(CallbackInfo ci) {
-		if (world.getRegistryKey().getValue().toString().equals("minecraft:the_nether") && !BlockProximity.isSafe(this.getBlockPos(), world, Blocks.NETHER_PORTAL, 2) && !BlockProximity.isSafe(this.getBlockPos(),world, Blocks.NETHER_QUARTZ_ORE, 2)) {
+		if (world.getBiome(getBlockPos()).isIn(HOT) && !BlockProximity.isSafe(getBlockPos(), world)) {
 			applyDamage(DamageSource.OUT_OF_WORLD, 0.05f);
-			counter +=1;
-			if(counter == 10) {
+			counter += 1;
+			counter %= 10;
+			if(counter == 0) {
 				playSound(SoundEvents.ENTITY_GENERIC_EXTINGUISH_FIRE, 0.5F, 1F);
-				counter = 0;
 			}
 		}
 	}
