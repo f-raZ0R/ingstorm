380b614f11ccd3fc9d38200939cb9f9f9f6f1763
f-raZ0R <MonkeyCityOwner@gmail.com>
2022-08-18 16:24:59 -0400

    Revamped passive damage to now handle absorption hearts properly (hardcoded to specifically only work with Absorption, does not handle things like Spectrum Azure Dike), make it only actually try to damage you if you are capable of being damaged, so no more constant damage tilt in creative mode. Damage tilt still happens for reasons that only god knows.

--- src/main/java/net/fraZ0R/ingstorm/mixin/MixinPlayerEntity.java
+++ src/main/java/net/fraZ0R/ingstorm/mixin/MixinPlayerEntity.java
@@ -4,6 +4,7 @@ import net.fraZ0R.ingstorm.Ingstorm;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.stat.Stats;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -18,6 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.Inject;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+//import net.minecraft.stat.Stat;
 import net.fraZ0R.ingstorm.common.BlockProximity;
 
 @Mixin(PlayerEntity.class)
@@ -25,6 +27,12 @@ public abstract class MixinPlayerEntity extends LivingEntity {
 	@Shadow public abstract void playSound(SoundEvent sound, float volume, float pitch);
 
 	@Shadow public abstract double getHeightOffset();
+	@Shadow public abstract void increaseStat(Identifier stat, int amount);
+
+	@Shadow public abstract boolean isInvulnerableTo(DamageSource damageSource);
+
+	//todo: make this variable be taken from a config file instead of hardcoded to 0.05
+	public float corDamage = 0.05f;
 
 	@Unique int counter = 0;
 	/**
@@ -41,9 +49,25 @@ public abstract class MixinPlayerEntity extends LivingEntity {
 
 	@Inject(method = "tick", at = @At("HEAD"))
 	private void doNetherDamage(CallbackInfo ci) {
+		//Damage source type is temporarily starvation.
+		if (canTakeDamage() && world.getBiome(getBlockPos()).isIn(CORROSIVE) && !(BlockProximity.isSafe(getBlockPos(), world) || BlockProximity.isSafe(getBlockPos().up((int)getStandingEyeHeight()), world))) {
+			//semi-hacky way to avoid damage tilt by not even calling the damage function which makes the tilt in the first place.
+			float realDamage = Math.max(corDamage-getAbsorptionAmount(), 0.0f);
+			float oldAbs = getAbsorptionAmount();
+			setAbsorptionAmount(Math.max(getAbsorptionAmount()-corDamage, 0.0f));
+			increaseStat(Stats.DAMAGE_ABSORBED, Math.round((oldAbs-getAbsorptionAmount())*10));
+			if(realDamage >= getHealth())
+			{
+				damage(DamageSource.STARVE, realDamage);
+			}
+			else
+			{
+				setHealth(getHealth() - realDamage);
+				increaseStat(Stats.DAMAGE_TAKEN, Math.round(realDamage*10));
+			}
 
-		if (world.getBiome(getBlockPos()).isIn(CORROSIVE) && !(BlockProximity.isSafe(getBlockPos(), world) || BlockProximity.isSafe(getBlockPos().up((int)getStandingEyeHeight()), world))) {
-			applyDamage(DamageSource.OUT_OF_WORLD, 0.05f);
+
+
 			counter += 1;
 			counter %= 10;
 			if(counter == 0) {
